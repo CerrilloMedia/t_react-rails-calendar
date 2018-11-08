41c1bec2509fd4b6757a3498a0edf3cbb815ee64
Armando Cerrillo <armando@cerrillomedia.org>
2018-11-08 14:42:41 -0800

    add state change for title & datetime inputs

--- app/assets/javascripts/components/appointmentForm.js.jsx
+++ app/assets/javascripts/components/appointmentForm.js.jsx
@@ -2,8 +2,10 @@ class AppointmentForm extends React.Component {
   render() {
     return (
         <form>
-          <input name="title" placeholder="appointment Title" />
-          <input name="appt_time" placeholder="appointment date and time" />
+          <input name="title" placeholder="Appointment Title"
+                 value={this.props.input_title} onChange={this.props.handleTitleChange } />
+          <input name="appt_time" placeholder="Appointment date and time"
+                 value={this.props.input_appt_time} onChange={this.props.handleApptTimeChange } />
           <input type="submit" value='Make Appointment' />
         </form>
     )
--- app/assets/javascripts/components/appointments.js.jsx
+++ app/assets/javascripts/components/appointments.js.jsx
@@ -1,9 +1,35 @@
 class Appointments extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointments: this.props.appointments,
+      input_title: 'Appointment Title',
+      input_appt_time: 'Tomorrow at 9am'
+    }
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handleApptTimeChange = this.handleApptTimeChange.bind(this)
+  }
+
+  handleTitleChange(e) {
+    this.setState({
+      input_title: e.target.value
+    })
+  }
+
+  handleApptTimeChange(e) {
+    this.setState({
+      input_appt_time: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        <AppointmentForm />
-        <AppointmentsList appointments={this.props.appointments} />
+        <AppointmentForm input_title={this.state.input_title} handleTitleChange={ this.handleTitleChange }
+                         input_appt_time={this.state.input_appt_time} handleApptTimeChange={ this.handleApptTimeChange }
+                         />
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
